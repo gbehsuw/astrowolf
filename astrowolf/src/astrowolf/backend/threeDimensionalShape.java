0977264033e0c6e45c3b03735ca262e786274e51
ddrtm <ddrtm@RyanLenovo>
2025-02-18 11:03:18 -0500

    sigma boy in ohio fanum taxed kai cenat with livy dunne. skibidi toilet kaiju vs camera man kaiju. The weather outside is rizzy but the fire is so skibidi but since ive gyatt to go ohio ohio ohio

--- astrowolf/src/astrowolf/backend/threeDimensionalShape.java
+++ astrowolf/src/astrowolf/backend/threeDimensionalShape.java
@@ -3,16 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package astrowolf.backend;
-import java.awt.*;
+import java.awt.Polygon;
+import java.awt.Graphics;
 /**
  *
  * @author mithu
  */
-public class threeDimensionalShape extends Polygon {
+public class threeDimensionalShape {
     private int[] z;
     private static int paneZ=100;
     private static int observerX=Const.screenWidth/2;
     private static int observerY=Const.screenHeight/2;
     private static int observerZ=0;
+    private Polygon twoD;
+    private Polygon threeDPoly=new Polygon();
     
+    public void update(){
+        threeDPoly.reset();
+        for(int i=0;i<z.length;i++){
+        int xSlope=(z[i]-observerZ)/(observerX-twoD.xpoints[i]);
+        int ySlope=(z[i]-observerZ)/(observerY-twoD.ypoints[i]);
+        threeDPoly.addPoint(paneZ/xSlope, paneZ/ySlope);
+        }
+    }
+    public void draw(Graphics g){
+        update();
+        g.drawPolygon(threeDPoly);
+    }
 }
